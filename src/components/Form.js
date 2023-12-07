2ca7e5758e7e20b786c694d007644d009be21107
ZAX20 <minhtatkhant6529@gmail.com>
2023-12-08 02:07:19 +0630

    already todoList web

---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import styles from "../style.module.css";
+import shortid from "shortid";
+
+const Form = ({todo, setTodo, todoList, setTodoList}) => {
+    const handleChange = (event) => {
+        setTodo(event.target.value);
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setTodoList([...todoList, {name:todo,id:shortid.generate()}]);
+        console.log(todoList);
+        setTodo("");
+    }
+    return(
+        <div className={styles.todoForm}>
+            <form onSubmit={handleSubmit}>
+                <input value={todo} onChange={handleChange} className={styles.todoInput} placeholder="Add Todo List"></input>
+                <button type="submit" className={styles.todoButton}>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import styles from "../style.module.css";
+const Header = () => {
+    return <div className={styles.header}>Today Todo-List</div>;
+}
+
+export default Header;
